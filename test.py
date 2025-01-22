05e59812b051b9d14081fdb4e5ea847213a1ad31
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-22 10:32:07 +0700

    first initialization

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create a NumPy array
+numbers = np.array([1, 2, 3, 4, 5])
+
+# Multiply all elements by 2
+doubled = numbers * 2
+
+print("Original:", numbers)
+print("Doubled:", doubled)
